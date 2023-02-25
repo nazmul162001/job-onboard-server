bdf9ffff657e01b79972b29345c7f4d327857f4b
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2023-02-25 18:51:50 +0600

    update database

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@teamcodesamurai.adbl3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.odh3cwx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -7,3 +7,14 @@ const client = new MongoClient(uri, {
 });
 
 module.exports = client;
+
+//======================== Backup Previous ======================//
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@teamcodesamurai.adbl3.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
+// module.exports = client;
