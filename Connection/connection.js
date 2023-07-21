85d61ca3ad7cf917cd1da6b70c4b55e02566621e
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-21 16:39:11 +0600

    update backend

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,5 +1,11 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.odh3cwx.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.odh3cwx.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vlbu0d.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
